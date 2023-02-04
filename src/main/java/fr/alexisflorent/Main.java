36f77465aa7a5a94bd7cc39e1074e761dda86b1e
ShAd <alexis.tatarkovic@gmail.com>
2023-02-04 17:05:23 +0100

    wip

--- src/main/java/fr/alexisflorent/Main.java
+++ src/main/java/fr/alexisflorent/Main.java
@@ -8,14 +8,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
         // Connexion à la base de données
-        String url = "jdbc:mysql://localhost:3306/java_jdbc";
+        String url = "jdbc:mysql://localhost:3306/mabd";
         String user = "root";
-        String password = "";
-        Connection con = null;
+        String password = "root";
+        Connection con;
         try {
             System.out.println("Connexion à la base de données réussi");
             con = DriverManager.getConnection(url, user, password);
@@ -37,6 +40,7 @@ public class Main {
             return;
         }
 
+        // Boucle infinie pour ne pas arrêter le serveur
         while (true) {
             try {
                 // Attente d'une connexion entrante
@@ -45,28 +49,50 @@ public class Main {
 
                 // Réception des données
                 ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-                Object object = ois.readObject();
+                Object livres = ois.readObject();
+                Object lecteurs = ois.readObject();
 
-                // Stockage des données dans la base de données
-                // if (object instanceof Book) {
-                     String sql = "INSERT INTO books (title, author) VALUES (?, ?)";
-                     PreparedStatement statement = con.prepareStatement(sql);
-                     statement.setString(1, "booktitle");
-                     statement.setString(2, "bookauthor");
-                     statement.executeUpdate();
-                // } else if (object instanceof Reader) {
-                //     Reader reader = (Reader) object;
-                //     String sql = "INSERT INTO readers (name, email) VALUES (?, ?)";
-                //     PreparedStatement statement = con.prepareStatement(sql);
-                //     statement.setString(1, "lecteurname");
-                //     statement.setString(2, "lecteurmail");
-                //     statement.executeUpdate();
-                // }
+                ArrayList<String[]> livresList = (ArrayList<String[]>) livres;
+                // Pour chaque livre dans la liste
+                for (String[] livre : livresList) {
+                    // On récupère les données : titre, auteur, nbPages
+                    String titre = livre[0];
+                    String auteur = livre[1];
+                    int nbPages = 0;
+                    try {
+                        nbPages = Integer.parseInt(livre[2]);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Le nombre de pages n'est pas un nombre, mais le livre est quand même enregistré (pages = 0)");
+                    }
+                    // On enregistre le livre dans la base de données
+                    String sql = "INSERT INTO livres (titre, auteur, nb_pages) VALUES (?, ?, ?)";
+                    PreparedStatement statement = con.prepareStatement(sql);
+                    statement.setString(1, titre);
+                    statement.setString(2, auteur);
+                    statement.setInt(3, nbPages);
+                    statement.executeUpdate();
+                }
+
+                ArrayList<String[]> lecteursList = (ArrayList<String[]>) lecteurs;
+                // Pour chaque lecteur dans la liste
+                for (String[] lecteur : lecteursList) {
+                    // On récupère les données : nom, prenom, email
+                    String nom = lecteur[0];
+                    String prenom = lecteur[1];
+                    String email = lecteur[2];
+                    // On enregistre le lecteur dans la base de données
+                    String sql = "INSERT INTO lecteurs (nom, prenom, email) VALUES (?, ?, ?)";
+                    PreparedStatement statement = con.prepareStatement(sql);
+                    statement.setString(1, nom);
+                    statement.setString(2, prenom);
+                    statement.setString(3, email);
+                    statement.executeUpdate();
+                }
 
                 // Fermeture de la connexion
                 ois.close();
                 socket.close();
-            } catch (IOException | ClassNotFoundException | SQLException e) {
+            } catch (IOException | SQLException | ClassNotFoundException e) {
                 e.printStackTrace();
             }
         }
